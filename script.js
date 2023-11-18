f385f17fe17f0cf264f70b6de689d90f31dd888c
Lorik Aliu <loriku123321@gmail.com>
2023-11-18 07:13:45 +0100

    Added and updated some new files

--- script.js
+++ script.js
@@ -38,71 +38,98 @@ function handleClick() {
   }
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   validateInputs();
 });
 
-const setError = (element, message) => {
+function setError(element, message) {
   const inputControl = element.parentElement;
   const errorDisplay = inputControl.querySelector(".error");
 
   errorDisplay.innerText = message;
   inputControl.classList.add("error");
   inputControl.classList.remove("success");
-};
+}
 
-const setSuccess = (element) => {
+function setSuccess(element) {
   const inputControl = element.parentElement;
   const errorDisplay = inputControl.querySelector(".error");
 
   errorDisplay.innerText = "";
   inputControl.classList.add("success");
   inputControl.classList.remove("error");
-};
+}
 
-const isValidEmail = (email) => {
+function isValidEmail(email) {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
-};
+}
 
-const validateInputs = () => {
-  const usernameValue = username.value.trim();
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
-  const password2Value = password2.value.trim();
+function redirectToSuccess() {
+  window.location.href = "success.html";
+}
+
+function validateInputs() {
+  const usernameValue = document.getElementById("username").value.trim();
+  const emailValue = document.getElementById("email").value.trim();
+  const passwordValue = document.getElementById("password").value.trim();
+  const password2Value = document.getElementById("password2").value.trim();
 
   if (usernameValue === "") {
-    setError(username, "Username is required");
+    setError(document.getElementById("username"), "Username is required");
   } else {
-    setSuccess(username);
+    setSuccess(document.getElementById("username"));
   }
 
   if (emailValue === "") {
-    setError(email, "Email is required");
+    setError(document.getElementById("email"), "Email is required");
   } else if (!isValidEmail(emailValue)) {
-    setError(email, "Provide a valid email address");
+    setError(document.getElementById("email"), "Provide a valid email address");
   } else {
-    setSuccess(email);
+    setSuccess(document.getElementById("email"));
   }
 
   if (passwordValue === "") {
-    setError(password, "Password is required");
+    setError(document.getElementById("password"), "Password is required");
   } else if (passwordValue.length < 8) {
-    setError(password, "Password must be at least 8 characters");
+    setError(
+      document.getElementById("password"),
+      "Password must be at least 8 characters"
+    );
   } else {
-    setSuccess(password);
+    setSuccess(document.getElementById("password"));
   }
 
   if (password2Value === "") {
-    setError(password2, "Please confirm your password");
+    setError(
+      document.getElementById("password2"),
+      "Please confirm your password"
+    );
   } else if (password2Value !== passwordValue) {
-    setError(password2, "Passwords don't match");
+    setError(document.getElementById("password2"), "Passwords don't match");
   } else {
-    setSuccess(password2);
+    setSuccess(document.getElementById("password2"));
   }
-};
+
+  const usernameValid = document
+    .getElementById("username")
+    .parentElement.classList.contains("success");
+  const emailValid = document
+    .getElementById("email")
+    .parentElement.classList.contains("success");
+  const passwordValid = document
+    .getElementById("password")
+    .parentElement.classList.contains("success");
+  const password2Valid = document
+    .getElementById("password2")
+    .parentElement.classList.contains("success");
+
+  if (usernameValid && emailValid && passwordValid && password2Valid) {
+    redirectToSuccess();
+  }
+}
 
 function toggleRegisterForm() {
   const registerForm = document.getElementById("registerForm");
@@ -128,4 +155,54 @@ function changeButtonColor() {
 const submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click", () => {
   changeButtonColor();
+});
+
+const aboutLink = document.querySelector('a[href="about.html"]');
+aboutLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "about.html";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const servicesLink = document.getElementById("servicesLink");
+  const ourServices = document.getElementById("ourServices");
+
+  servicesLink.addEventListener("click", function (event) {
+    event.preventDefault();
+    smoothScrollTo(ourServices);
+  });
+
+  function smoothScrollTo(target) {
+    const targetPosition = target.getBoundingClientRect().top + window.scrollY;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    const duration = 1000;
+    let start = null;
+
+    function animation(currentTime) {
+      if (start === null) {
+        start = currentTime;
+      }
+      const timeElapsed = currentTime - start;
+      const scroll = easeInOutQuad(
+        timeElapsed,
+        startPosition,
+        distance,
+        duration
+      );
+      window.scrollTo(0, scroll);
+      if (timeElapsed < duration) {
+        requestAnimationFrame(animation);
+      }
+    }
+
+    function easeInOutQuad(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  }
 });
